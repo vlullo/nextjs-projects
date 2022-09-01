07347c6d9f0f9d9a50d144a591b7b65f61d4b4c8
Valentina Lullo <valentina.lullo@reedonline.co.uk>
2022-09-01 22:43:19 +0100

    Adding example of Static generation

---
+++ 03-static-generation/pages/index.js
@@ -0,0 +1,39 @@
+import fs from "fs/promises";
+import path from "path";
+
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  //Redirect optional
+  if (!data) {
+    return { redirect: { destination: "/no-data" } };
+  }
+
+  //notFound optional
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
